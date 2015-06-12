de4a19f71a159faea749332e74ad82642607a3de
Zell Liew <zellwk@gmail.com>
2015-06-12 19:11:41 +0800

    Zell's changes for Masonry

--- app/js/gallery.js
+++ app/js/gallery.js
@@ -1,21 +1,17 @@
-/*http://masonry.desandro.com/*/
-$(function(){
-    $('.responsive').picture();
-});
-	/*Masonry*/
+/*Masonry*/
 $(document).ready(function($) {
   var $masonryContainer = $('.photos');
-$masonryContainer.imagesLoaded(function() {
-	    var columns    = 3,
-	        setColumns = function() { columns = $( window ).width() > 1500 ? 4 : $( window ).width() > 640 ? 3 : $( window ).width() > 320 ? 2 : 1; };
 
-	    setColumns();
-	    $( window ).resize( setColumns );
+  var columns;
+  var windowWidth = $(window).width();
 
-	    $( '#column' ).masonry(
-	    {
-	        itemSelector: '.responsive',
-	        columnWidth:  function( containerWidth ) { return containerWidth / columns; }
-	    });
-	});
+  $masonryContainer.imagesLoaded(function() {
+    $masonryContainer.masonry({
+      itemSelector: '.responsive',
+      columnWidth: '.column',
+      gutter: '.gutter'
+    });
+
+    console.log($masonryContainer);
+  });
 });
